7cf20a0dc97e7ef26fe89acc3ccc83461dc8a51e
Prasad-Sable <prasadsable2000@gmail.com>
2024-07-22 22:33:15 +0530

    tenth

--- server.js
+++ server.js
@@ -23,11 +23,7 @@ dbConnect();
 
 // Middleware
 app.use(helmet());
-app.use(cors({
-  origin: process.env.CLIENT_URI,
-  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods if needed
-  credentials: true, // Enable credentials if needed
-}));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
