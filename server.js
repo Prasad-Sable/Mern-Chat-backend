d0d85c530c1027f0ea63bd38e9b33a6c4f0578cb
Prasad-Sable <prasadsable2000@gmail.com>
2024-07-22 22:39:08 +0530

    tenth

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.static())
 app.use(express.json());
 
 // Routes
